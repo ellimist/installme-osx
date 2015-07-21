d728fbd04502ab2c3980c5a00aa7b87fa5784b47
Adrian Bordinc <adrian@airtame.com>
2015-07-21 20:23:08 +0200

    Personal config

--- setup.sh
+++ setup.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
+# Install XCode Command Line Tools
+xcode-select --install
 
 # setup folder structure
 mkdir ~/Documents/workspace
 mkdir ~/Documents/repos
 
+sudo gem update --system
+sudo chown -R $USER /Library/Ruby /Library/Perl /Library/Python
+
+echo "gem: -n/usr/local/bin" >> ~/.gemrc
 
 # brew setup
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
 brew upgrade
+
+# install Rails
+gem install rails
+
+# ZSH
+curl -L github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # Core utils
 brew install coreutils
@@ -22,10 +34,16 @@ brew install optipng
 brew install readline
 
 # Apps
+brew install ack
+brew install thefuck
+brew install bash-completion
 brew install the_silver_searcher
 brew install ant
 brew install git
+brew install git-flow
 brew install mercurial
+brew install mysql
+brew install postgresql
 brew install node
 brew install irssi
 brew install phantomjs
@@ -33,12 +51,19 @@ brew install unrar
 brew install trash
 brew install tree
 brew install wget
+
+wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
+wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+
 brew install tig
 brew install ffind
 
-# MacVim for terminal
-brew install macvim
-alias vim='mvim -v'
+gem install passenger
+brew install nginx --with-passenger --with-debug --with-spdy --with-gunzip \
+  && sudo mkdir -p /Library/LaunchDaemons \
+  && sudo cp -r Config/nginx /usr/local/etc/nginx \
+  && sudo cp LaunchDaemons/homebrew.mxcl.nginx.plist /Library/LaunchDaemons/ \
+  && sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
 
 # OSX native apps
 brew install caskroom/cask/brew-cask
@@ -48,7 +73,6 @@ function installcask() {
 }
 
 # dev-related programs
-installcask atom
 installcask dropbox
 installcask copy
 installcask google-chrome
@@ -57,22 +81,13 @@ installcask iterm2
 
 # utils
 installcask skype
-installcask lastfm
 installcask vlc
 installcask 1password
-installcask camtasia
-installcask cleanmymac
-installcask macpaw-gemini
-installcask flickr-uploadr
 installcask spotify
-installcask licecap
-installcask skitch
 installcask slack
-
-# games
-installcask steam
-installcask minecraft
-installcask openttd
+installcask flux
+installcask transmission
+installcask sequel-pro
 
 
 # npm dependencies that I'm not likely to live without
@@ -92,25 +107,23 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 cd ~/.vim/bundle/
 
 # Get all plugins
-git clone https://github.com/vim-scripts/bufkill.vim
-git clone https://github.com/wincent/Command-T.git
-git clone https://github.com/tpope/vim-fugitive.git
-echo "Command-T will need extra setup, please review its docs"
-open https://github.com/wincent/Command-T/blob/master/doc/command-t.txt#L174
-git clone https://github.com/rking/ag.vim
-git clone https://github.com/editorconfig/editorconfig-vim.git
-git clone https://github.com/sjl/gundo.vim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/mitechie/pyflakes-pathogen.git
+git clone https://github.com/Valloric/MatchTagAlways.git
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/alvan/vim-closetag
+git clone https://github.com/kien/ctrlp.vim.git ctrlp.vim
+git clone https://github.com/mattn/emmet-vim.git
 git clone https://github.com/scrooloose/syntastic.git
-echo "Syntastic will rely on code validation tools, such as jshint"
-git clone https://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/Lokaltog/vim-easymotion.git
-git clone https://github.com/nvie/vim-flake8.git
-git clone https://github.com/mhinz/vim-signify.git
-git clone https://github.com/millermedeiros/vim-statline.git
-git clone https://github.com/AndrewRadev/splitjoin.vim.git
-git clone https://github.com/godlygeek/tabular.git
+git clone git://github.com/godlygeek/tabular.git
+git clone https://github.com/vim-scripts/tlib.git
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/tpope/vim-bundler.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git
 git clone https://github.com/elzr/vim-json.git
 git clone https://github.com/ruyadorno/vim-change-indent.git
 git clone https://github.com/groenewege/vim-less.git
@@ -120,36 +133,21 @@ git clone https://github.com/othree/html5.vim.git
 git clone https://github.com/pangloss/vim-javascript.git
 git clone https://github.com/nathanaelkane/vim-indent-guides.git
 
-
-# Configure Command-T
-cd ~/.vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
-
-
 ## Get dotfiles repo
 cd ~/Documents/repos/
-git clone https://github.com/ruyadorno/dotfiles.git
-
+git clone https://github.com/ellimist/dotfiles.git
 
 ## Get zsh bundle manager
 git clone https://github.com/tarjoilija/zgen.git
 
-
 ## Setup symlinks
 cd ~/
-ln -s Documents/repos/dotfiles/bashrc .bash_profile
-ln -s Documents/repos/dotfiles/gitconfig .gitconfig
-ln -s Documents/repos/dotfiles/gitignore_global .gitignore_global
-ln -s Documents/repos/dotfiles/hgrc .hgrc
-ln -s Documents/repos/dotfiles/vimrc .vimrc
-ln -s Documents/repos/dotfiles/zshrc .zshrc
-
+ln -s Documents/repos/dotfiles/.gitconfig .gitconfig
+ln -s Documents/repos/dotfiles/.vimrc .vimrc
+ln -s Documents/repos/dotfiles/.vim .vim
+ln -s Documents/repos/dotfiles/.zshrc .zshrc
 
 # Set zsh as default shell
 chsh -s /bin/zsh
 
-
-## Casks that needs password permission
-installcask private-internet-access
-
+echo 'All done!'
